b6f06f023ad1d1832aff9780a54704659720008b
akuaki96 <akuaki96@outlook.jp>
2023-05-11 23:42:43 +0900

    feat/投稿内容を反映する機能追加

---
+++ pages/datas/headerData.tsx
@@ -0,0 +1,23 @@
+type headerData = {
+  title: String;
+  link: String;
+};
+
+export const headerData: headerData[] = [
+  {
+    title: "TOP",
+    link: "./",
+  },
+  {
+    title: "投稿",
+    link: "./",
+  },
+  {
+    title: "ユーザー情報",
+    link: "./",
+  },
+  {
+    title: "お問い合わせ",
+    link: "./",
+  },
+];
