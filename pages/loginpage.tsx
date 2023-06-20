e45a7066ea4f88e0065efe39e51c35c8615e8950
akuaki96 <akuaki96@outlook.jp>
2023-06-20 17:46:17 +0900

    feat:ログインのAPIをたたく

--- pages/loginpage.tsx
+++ pages/loginpage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import style from "../styles/LoginPage.module.css";
 
 const Loginpage = () => {
@@ -16,37 +16,72 @@ const Loginpage = () => {
     },
   ]);
 
-  const onclickSubmit = () => {
-    setUserInfo((prev) => [...prev, { name: inputUsername, pass: inputPass }]);
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    //formを使うときリロードを防ぐためのコード
+    try {
+      // const res = await((await fetch("http://localhost:3001/login")).json())
+      const res = await fetch("http://localhost:3001/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({ email: inputUsername, password: inputPass }),
+      });
+
+      const json = await res.json();
+      console.log(json);
+
+      setUserInfo((prev) => [
+        ...prev,
+        { name: inputUsername, pass: inputPass },
+      ]);
+
+      console.log(event);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
-  console.log(userInfo);
+  // email: test@example.com pass:testPass
+  // ユーザー名　cypasman
+
+  //   console.log(userInfo);
 
   return (
     <div className={style.loginform}>
       <div className={style.loginArea}>
         <h1 className={style.loginTitle}>Login</h1>
-        <div className={style.inputUsernameArea}>
-          <input
-            className={style.inputUsername}
-            value={inputUsername}
-            onChange={(e) => setInputUsername(e.target.value)}
-            placeholder="username"
-          ></input>
-        </div>
 
-        <div className={style.inputPasswordArea}>
-          <input
-            className={style.inputPass}
-            value={inputPass}
-            onChange={(e) => setInputPass(e.target.value)}
-            placeholder="password"
-          ></input>
-        </div>
+        <form onSubmit={onSubmit}>
+          <div className={style.inputUsernameArea}>
+            <input
+              className={style.inputUsername}
+              value={inputUsername}
+              onChange={(e) => setInputUsername(e.target.value)}
+              placeholder="username"
+              required
+              type="email"
+              // minLength={5}
+            ></input>
+          </div>
 
-        <div className={style.loginButton}>
-          <button onClick={onclickSubmit}>Login</button>
-        </div>
+          <div className={style.inputPasswordArea}>
+            <input
+              className={style.inputPass}
+              value={inputPass}
+              onChange={(e) => setInputPass(e.target.value)}
+              placeholder="password"
+              required
+              type="password"
+            ></input>
+          </div>
+
+          <div className={style.loginButton}>
+            <button>Login</button>
+          </div>
+        </form>
       </div>
     </div>
   );
